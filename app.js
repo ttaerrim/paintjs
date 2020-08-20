b3997332cb3bf5a8b24f0e47cd46e8764bebfc67
Lee <ltr0121@naver.com>
2020-08-20 21:30:49 +0900

    선 그리기 기능 구현

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 const canvas = document.getElementById('jsCanvas');
+const ctx = canvas.getContext('2d'); //픽셀 컨트롤
+
+//canvas 사이즈 설정해야 함 그래야 그려짐
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = 'black';
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,9 +14,21 @@ function stopPainting() {
     painting = false;
 }
 
+function startPainting() {
+    painting = true;
+}
+
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if (!painting) {
+        ctx.beginPath(); //path는 line
+        ctx.moveTo(x, y);
+    } else {
+        // 마우스를 움직이는 내내 발생
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 function onMouseDown(event) {
@@ -22,7 +42,7 @@ function onMouseUp(event) {
 
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
-    canvas.addEventListener('mousedown', onMouseDown);
-    canvas.addEventListener('mouseup', onMouseUp);
+    canvas.addEventListener('mousedown', startPainting);
+    canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
 }
