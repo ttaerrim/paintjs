d76444af9af75da6444b5b8b8c6a1fcd63eecbda
Lee <ltr0121@naver.com>
2020-08-21 20:00:44 +0900

    브러쉬 사이즈 변경 적용 & Fill/Paint 버튼

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext('2d'); //픽셀 컨트롤
+const colors = document.getElementsByClassName('jsColor');
+const range = document.getElementById('jsRange');
+const mode = document.getElementById('jsMode');
 
 //canvas 사이즈 설정해야 함 그래야 그려짐
 canvas.width = 500;
@@ -9,6 +12,7 @@ ctx.strokeStyle = 'black';
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPainting() {
     painting = false;
@@ -31,11 +35,6 @@ function onMouseMove(event) {
     }
 }
 
-function onMouseDown(event) {
-    painting = true;
-    console.log(event);
-}
-
 function onMouseUp(event) {
     stopPainting();
 }
@@ -45,4 +44,37 @@ if (canvas) {
     canvas.addEventListener('mousedown', startPainting);
     canvas.addEventListener('mouseup', stopPainting);
     canvas.addEventListener('mouseleave', stopPainting);
+}
+
+function handleColor(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
+
+function handleRangeChange(event) {
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick() {
+    if (filling == true) {
+        filling = false;
+        mode.innerText = "Fill";
+    } else {
+        filling = true;
+        mode.innerText = 'Paint';
+    }
+}
+
+if (colors) {
+    Array.from(colors).forEach(color => color.addEventListener('click', handleColor));
+    // colors는 배열이 아니라서 (HTMLCollection인가 그거임) 배열로 바꿔 주고 forEach로 모든 색당 하나씩 이벤트리스너 달아줌
+}
+
+if (range) {
+    range.addEventListener('input', handleRangeChange);
+}
+
+if (mode) {
+    mode.addEventListener('click', handleModeClick);
 }
